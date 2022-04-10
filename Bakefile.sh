2611812352c79db659ce1365fd120e3eaa07abed
Edwin Kofler <edwin@kofler.dev>
2022-04-10 12:01:39 -0700

    feat: Add '-v' flag to print verison. Closes #4

--- Bakefile.sh
+++ Bakefile.sh
@@ -7,3 +7,11 @@ task.test() {
 task.docs() {
 	shdoc < './pkg/src/bakeScript.sh' > './docs/api.md'
 }
+
+task.release() {
+	local version="$1"
+	bake.assert_not_empty 'version'
+
+	sed -ie "s|\(\t\tlocal bake_version='\)\(.*\)\('.*\)|\1$version\3|" './pkg/src/bakeScript.sh'
+	basalt releases "$version"
+}
