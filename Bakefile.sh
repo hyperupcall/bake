54092d64645a375d170a569f83f8191904204e4b
Edwin Kofler <edwin@kofler.dev>
2022-10-21 23:48:28 -0700

    rename(3/3): Fix name references

--- Bakefile.sh
+++ Bakefile.sh
@@ -9,7 +9,7 @@ init() {
 # watch: --ignore **/docs/*
 # config: big-print=off
 task.docs() {
-	shdoc < './pkg/src/bakeScript.sh' > './docs/api.md'
+	shdoc < './pkg/bin/bake' > './docs/api.md'
 }
 
 # doc: release information
@@ -18,8 +18,8 @@ task.release() {
 	bake.assert_not_empty 'version'
 
 	if basalt release "$version"; then
-		sed -i "s|\(\t\tlocal bake_version='\)\(.*\)\('.*\)|\1$version\3|" './pkg/src/bakeScript.sh'
-		git add './pkg/src/bakeScript.sh'
+		sed -i "s|\(\t\tlocal bake_version='\)\(.*\)\('.*\)|\1$version\3|" './pkg/bin/bake'
+		git add './pkg/bin/bake'
 		git commit --amend --no-edit
 	fi
 }
@@ -27,7 +27,7 @@ task.release() {
 task.release-post() {
 	local version{1,2}=
 
-	version1=$(sed -n "s|\t\tlocal bake_version='\(.*\)'.*|\1|p" './pkg/src/bakeScript.sh')
+	version1=$(sed -n "s|\t\tlocal bake_version='\(.*\)'.*|\1|p" './pkg/bin/bake')
 	version2=$(sed -n "s|version = '\(.*\)'|\1|p" './basalt.toml')
 
 	if [ "$version1" != "$version2" ]; then
