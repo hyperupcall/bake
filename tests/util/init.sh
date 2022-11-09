053d0b8faafb6c2abf4209b63a93bc9b72b1866d
Edwin Kofler <edwin@kofler.dev>
2022-11-08 23:46:24 -0800

    test: Cleanup cruft

--- tests/util/init.sh
+++ tests/util/init.sh
@@ -6,9 +6,6 @@ basalt.load 'github.com/hyperupcall/bats-all' 'load.bash'
 
 bats_require_minimum_version 1.7.0
 
-load './util/test_utils.sh'
-
-BAKE_INTERNAL_CAN_SOURCE='yes' BAKE_INTERNAL_TEST='yes' source "$BASALT_PACKAGE_DIR/pkg/bin/bake"
 bake() { "$BASALT_PACKAGE_DIR/pkg/bin/bake" "$@"; }
 
 setup() {
