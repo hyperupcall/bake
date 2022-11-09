9390f0da8bf2a4e75743923e89b928d216fafbb0
Edwin Kofler <edwin@kofler.dev>
2022-11-09 01:07:27 -0800

    fix: Version now has identifier for dev version. Closes #33

--- Bakefile.sh
+++ Bakefile.sh
@@ -11,30 +11,3 @@ init() {
 task.docs() {
 	shdoc < './pkg/bin/bake' > './docs/api.md'
 }
-
-# doc: release information
-task.release() {
-	local version="$1"
-	bake.assert_not_empty 'version'
-
-	if basalt release "$version"; then
-		sed -i "s|\(\t\tlocal bake_version='\)\(.*\)\('.*\)|\1$version\3|" './pkg/bin/bake'
-		git add './pkg/bin/bake'
-		git commit --amend --no-edit
-	fi
-}
-
-task.release-post() {
-	local version{1,2}=
-
-	version1=$(sed -n "s|\t\tlocal bake_version='\(.*\)'.*|\1|p" './pkg/bin/bake')
-	version2=$(sed -n "s|version = '\(.*\)'|\1|p" './basalt.toml')
-
-	if [ "$version1" != "$version2" ]; then
-		bake.die "Version '$version1' and version '$version2' do not match"
-	fi
-}
-
-task.run1() {
-	printf '%s\n' "$*"
-}
